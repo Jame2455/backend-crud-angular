899fda0516bf064ccef4589a06927283faa5fedb
Phongsakorn1 <163574329+Phongsakorn1@users.noreply.github.com>
2024-03-16 01:21:42 +0700

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -18,6 +18,7 @@ const getUsers = async (req, res) => {
         console.log('hi im fah7')
         console.log('hi im fah888')
         console.log('hi im game2')
+        console.log('hi im tae11')
         return res.status(200).send(rows);
     } catch (error) {
         console.log(error);
